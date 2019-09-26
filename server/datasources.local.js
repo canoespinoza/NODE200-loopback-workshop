4dadf93413cc65d57ff31b496e9701b967e690e4
Cano Espinoza <kabacano@yahoo.com>
2019-09-25 23:28:41 -0700

    1-working loopback server tests

---
+++ server/datasources.local.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    "db": {
+      "name": "db",
+      "connector": "memory"
+    },
+    "MongoDB": {
+      "url": "mongodb://localhost:27017",
+      "name": "MongoDB",
+      "connector": "mongodb"
+    
+    }
+
+}
